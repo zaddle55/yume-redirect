9e5b255a7a064358240df3e62827e9a0492bfe2d
Zaddle <chenyifan20050115@126.com>
2024-08-02 05:04:32 +0800

    feature: a surprise redirection triggered by specific seed

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,18 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 // import { exec } from 'child_process';
 import { readFileSync } from 'fs';
 
-const SPECIAL_URLS: string = "";
-const SPECIAL_SEED: number = 0;
+const SPECIAL_URLS: string = "https://b23.tv/bWUfYA0";
+const SPECIAL_SEED: string = "i83";
+
+const rand = (seed: string): number => {
+    let hash: number = 0;
+    for (let i = 0; i < seed.length; i++) {
+        hash = seed.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const x: number = Math.sin((hash & 0x7FFFFFFF) % 100);
+
+    return x - Math.floor(x);
+}
 
 const redirecrt = (req: VercelRequest, res: VercelResponse) => {
     const args: string | string[] = req.query.seed;
@@ -13,23 +23,17 @@ const redirecrt = (req: VercelRequest, res: VercelResponse) => {
     }
 
     try {
-        // 解析seed参数
-        const seed: number = parseInt(args) || new Date().getTime();
-
-        console.log(`seed: ${seed}`);
-        
+        const seed: string = args || new Date().getTime().toString();
 
         let link: string = "";
 
         // 读取数据库json文件
-        // 获取vercel执行路径
         let path: string = process.cwd();
         const data: string = readFileSync(`${path}/database.json`, 'utf-8');
 
         const links: string[] = JSON.parse(data);
 
-        // 生成随机数
-        const random: number = Math.floor(Math.random() * links.length);
+        const random: number = Math.floor(rand(seed) * links.length);
         link = (seed===SPECIAL_SEED)? SPECIAL_URLS : links[random];
 
         // 调用python脚本
